9560c0048a657ec6067a0a6bfd91c270b2af477d
Aldo Utrera <accounts@aldoutrera.com>
2020-04-13 11:24:32 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+func handleRootPath(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.URL.Path == "/" {
+		// Load the index.html page
+	} else {
+		// Let's search the short url
+		shortcode := r.URL.Path
+		shortcode = strings.TrimPrefix(shortcode, "/")
+		w.Write([]byte(shortcode))
+	}
+}
+
+func createShortCode(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	w.Write([]byte("Creating the short code"))
+}
+
+func main() {
+	http.HandleFunc("/", handleRootPath)
+	http.HandleFunc("/shortcodes", createShortCode)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
